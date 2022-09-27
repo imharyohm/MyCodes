434c5f54b7203c2b579fec9df14fff4d9c132eff
imharyohm <imharyohm@gmail.com>
2022-09-27 13:51:53 +0530

    changed

--- Dsa_lab/lab2/ques1.c
+++ Dsa_lab/lab2/ques1.c
@@ -1,32 +1,29 @@
-/*1. Write a C program to find out maximum and minimum from an integer array.*/ 
+/*1. Write a C program to find out maximum and minimum from an integer array*/
 #include <stdio.h>
- #include <conio.h>
- 
- 
+
 int main()
 {
-    int i,n,min,max;
+    int i, n, min, max;
     int a[n];
-   
+
     printf("Enter size of the array : ");
-    scanf("%d",&n);
- 
+    scanf("%d", &n);
+
     printf("Enter elements in array : ");
-    for(i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d", &a[i]);
     }
-     min=max=a[0];
-    for(i=1; i<n; i++)
+    min = max = a[0];
+    for (i = 1; i < n; i++)
     {
-         if(min>a[i])
-		  min=a[i];   
-		   if(max<a[i])
-		    max=a[i];       
+        if (min > a[i])
+            min = a[i];
+        if (max < a[i])
+            max = a[i];
     }
-     printf("minimum element of array is : %d",min);
-          printf("\nmaximum element of array is : %d",max);
- 
- 
+    printf("minimum element of array is : %d", min);
+    printf("\nmaximum element of array is : %d", max);
+
     return 0;
 }
